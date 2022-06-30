97113fc104440c779f32b6f65beb64c6cb367fc1
R3n1t0 <98884076+R3n1t0@users.noreply.github.com>
2022-06-30 22:41:38 +0200

    update

--- app/Http/Controllers/ComicsController.php
+++ app/Http/Controllers/ComicsController.php
@@ -81,9 +81,13 @@ class ComicsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Comics $comic)
     {
-        //
+        $data = $request->all();
+        $data['slug'] = Str::slug($data['title'], '-');
+        $comic->update($data);
+
+        return redirect()->route('comics.show', $comic);
     }
 
     /**
--- resources/views/comics/edit.blade.php
+++ resources/views/comics/edit.blade.php
@@ -5,23 +5,25 @@
     <div class="container">
         <h2>Modifica di :{{ $comic->title }}</h2>
 
-        <form class="w-50" action="{{route('comics.edit')}}" method="POST">
+        <form class="w-50" action="{{route('comics.update')}}" method="POST">
 
             @csrf
+            @method('PUT')
 
             <div class="mb-3">
-              <label for="title" class="form-label">Titolo</label>
-              <input type="text" class="form-control" name='title' id="title" placeholder="Comic Title">
+                <label for="title" class="form-label">Titolo</label>
+                <input value="{{$comic->title}}" type="text" class="form-control" name='title' id="title" placeholder="Comic Title">
             </div>
 
             <div class="mb-3">
-              <label for="type" class="form-label">Tipolgia</label>
-              <input type="text" class="form-control" name='type' id="type" placeholder="Comic Type">
+                <label value="{{$comic->type}}" for="type" class="form-label">Tipolgia</label>
+                <input type="text" class="form-control" name='type' id="type" placeholder="Comic Type">
             </div>
 
             <div class="mb-3">
-              <label for="image" class="form-label">Url</label>
-              <input type="text" class="form-control" name='image' id="image" placeholder= "Comic Image Url">
+                <img class="w-25" src="{{$comic->image}}" alt="{{$comic->title}}">
+                <label for="image" class="form-label">Url</label>
+                <input value="{{$comic->image}}" type="text" class="form-control" name='image' id="image" placeholder= "Comic Image Url">
             </div>
 
             <button type="submit" class="btn btn-primary">Invia</button>
